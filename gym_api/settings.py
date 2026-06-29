16f3be6795d407eaa568f5e62bc8ca4ca9ad4a9f
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-06-29 18:29:41 -0300

    chore: add requirements.txt and enviroment

--- gym_api/settings.py
+++ gym_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!-77j=eu7bqq4*tb3(!xx9(kg%th@vg_%&^9^z6r)6v(zsfx=4'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
