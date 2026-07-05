e6cc1c0f12eeedb2cd55c242ca290db96d40c5cd
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-07-04 22:14:51 -0300

    feat(accounts): add Profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User
+from accounts.models import User, Profile
 
 class CustomUserAdmin(UserAdmin):
     model = User
@@ -8,3 +8,4 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ['email', 'username']
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+class RoleChoices(models.TextChoices):
+    ADMIN = "A", "Admin"
+    PERSONAL = "P", "Personal"
+    MEMBER = "M", "Member"
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=False, blank=False)
@@ -7,6 +13,17 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-
+    # Is active in User
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=1, choices=RoleChoices.choices, default=RoleChoices.MEMBER)
+    photo = models.ImageField(upload_to="media", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return f'{self.user.email} ({self.role})'
+    
