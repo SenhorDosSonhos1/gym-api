fef3e09f3d1fab40fe261a9b3bc0158e639130fd
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-07-05 22:03:38 -0300

    feat(academy): add exercise model

--- academy/admin.py
+++ academy/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from academy.models import Membership
+from academy.models import Membership, Exercise
 
 admin.site.register(Membership)
+admin.site.register(Exercise)
--- academy/models.py
+++ academy/models.py
@@ -6,6 +6,15 @@ class GymPlan(models.TextChoices):
     QUARTERLY = "T", "Trimestral"
     SEMIANNUAL = "S", "Semestral"
 
+class MuscleGroup(models.TextChoices):
+    CHEST = "PE", "Peito"
+    BACK = "C", "Costas"
+    SHOULDERS = "O", "Ombros"
+    ARMS = "B", "Braços"
+    LEGS = "P", "Pernas"
+    GLUTES = "G", "Gluteos"
+    CORE = "A", "Abdômen"
+
 class Membership(models.Model):
     profile = models.OneToOneField(Profile, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,3 +23,15 @@ class Membership(models.Model):
 
     def __str__(self):
         return f'Nome: {self.profile.user.email} | Plano: {self.plan}'
+    
+class Exercise(models.Model):
+    name = models.CharField(max_length=155)
+    description = models.TextField()
+    muscle_group = models.CharField(max_length=12, choices=MuscleGroup.choices)
+    photo = models.ImageField(upload_to="exercise/", null=True, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Exercicio: {self.name} | Parte: {self.muscle_group}"
